c92dda18bef46e15ebe6123625f65e30b0578186
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-22 22:31:24 -0600

    added acts_as_list gem for reording lists, created list model and table

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ group :test do
 end
 
 gem "active_model_serializers", "~> 0.10.12"
+
+gem 'acts_as_list', '~> 1.0', '>= 1.0.4'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_035721) do
+ActiveRecord::Schema.define(version: 2021_11_23_043037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
