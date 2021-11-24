3170259a54af19f8078a927ba9c5cfe9088e28aa
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-23 23:28:26 -0600

    working user creation and user sign in routes, no UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
 
      def create
-          user = User.find(params[:email])
+          user = User.find_by_email(params[:email])
           if user.authenticate(params[:password])
                session[:user_id] = user.id
                render json: user
           else 
-               render json: {error : "Failed to Log in"}
+               render json: {error: "Failed to Log in"}
           end
-
      end
+
 end
