cb67667992712d718bd7ac9969725493ee5572ad
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 11:24:35 -0600

    added flex display on navbar class

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Header() {
      return (
           <header className="fix-header">
-               <nav>
+               <nav className="navbar">
                <a href="/home"> <img id="nav-logo" src="https://i.ibb.co/XXRF8hy/2dc7b9004b6948018984e5c013ec7771.png" alt="Logo"/>
                Easy Boards
                </a>
                <div className="nav-right-buttons">
-                    <a className="nav-buttons"id="login-button" href="/login">Log In</a>
-                    <a className="nav-buttons"id="signup-button" href="/signup">Sign up</a>
+                    <a className="buttons"id="login-button" href="/login">Log In</a>
+                    <a className="buttons"id="signup-button" href="/signup">Sign up</a>
                </div>
                </nav>
           </header>
