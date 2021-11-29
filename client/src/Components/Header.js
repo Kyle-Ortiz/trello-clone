c07c0fbaba266929603b71b48130329a549efa43
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 10:33:19 -0600

    changed default font to match trello and created header nav component, new logo

---
+++ client/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+     return (
+          <header className="fix-header">
+               <nav>
+                    <a href="/home"> <img className="nav-logo" src="../logo.png" alt="Logo " />
+                    Easy Boards</a>
+                    <div>
+
+                    </div>
+               </nav>
+          </header>
+     )
+}
+
+export default Header
