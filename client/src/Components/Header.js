93a9be9f4ae8fb7e8cd3fd4353e4dbb1be2261e5
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 13:36:32 -0600

    added scroll listener that changes class name on fix-header, added gradient on home page

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {useState} from "react"
 
 function Header() {
+     const [navBar,setNavBar] = useState(false)
+
+     const scrollBackground = () => {
+          if (window.scrollY > 55) {
+               setNavBar(true)
+          } else {
+               setNavBar(false)
+          }
+     }
+
+     window.addEventListener('scroll', scrollBackground)
+
      return (
-          <header className="fix-header">
+          <header className={navBar ? "fix-header active" : "fix-header"}>
                <nav className="navbar">
                <a href="/home"> <img id="nav-logo" src="https://i.ibb.co/XXRF8hy/2dc7b9004b6948018984e5c013ec7771.png" alt="Logo"/>
                Easy Boards
