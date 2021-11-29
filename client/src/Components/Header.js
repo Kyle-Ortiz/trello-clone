e690038bed1a059c2285ac2007202ae8ca0e882b
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 10:47:05 -0600

    added image in header, removed hyperlink styling

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,11 +4,9 @@ function Header() {
      return (
           <header className="fix-header">
                <nav>
-                    <a href="/home"> <img className="nav-logo" src="../logo.png" alt="Logo " />
-                    Easy Boards</a>
-                    <div>
-
-                    </div>
+               <a href="/home"> <img id="nav-logo" src="https://i.ibb.co/XXRF8hy/2dc7b9004b6948018984e5c013ec7771.png" alt="Logo"/>
+               Easy Boards
+               </a>
                </nav>
           </header>
      )
