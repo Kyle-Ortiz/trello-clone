50e2f746a2994dfaeaeb6d12efa31dae06a14224
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 11:16:34 -0600

    created login and signup nav buttons, styled them to look like trello

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -7,6 +7,10 @@ function Header() {
                <a href="/home"> <img id="nav-logo" src="https://i.ibb.co/XXRF8hy/2dc7b9004b6948018984e5c013ec7771.png" alt="Logo"/>
                Easy Boards
                </a>
+               <div className="nav-right-buttons">
+                    <a className="nav-buttons"id="login-button" href="/login">Log In</a>
+                    <a className="nav-buttons"id="signup-button" href="/signup">Sign up</a>
+               </div>
                </nav>
           </header>
      )
