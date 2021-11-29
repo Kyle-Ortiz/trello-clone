47f79164049a67d57e86b91cdf162425ab3bf77e
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 13:52:27 -0600

    created signup component and imported to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Header from './Components/Header';
+import Signup from './Components/Signup'
+
 function App() {
   return (
     <div className="App">
---
+++ client/src/Components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Signup() {
+     return (
+          <div id="signup-form">
+               <div id="email-input">
+                    <h1>Sign up for your account</h1>
+               </div>
+          </div>
+     )
+}
+
+export default Signup
