e63906abe079d71ca673beb26ca957e3bba46cfd
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-29 14:41:54 -0600

    added stateful email form on signup page, basic right now

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import {useState} from 'react'
 function Signup() {
+     const [email,setEmail] = useState("")
 
      return (
           <div id="signup-form">
                <div id="email-input">
                     <h1>Sign up for your account</h1>
+                    <form action="submit">
+                         <input type="text" onChange={(e)=> setEmail(e.target.value)}placeholder="Enter Email" value={email}/>
+                    </form>
                </div>
           </div>
      )
