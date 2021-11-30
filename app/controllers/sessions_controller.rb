a80fac1c54164013fc1a0f636829451aa96f6bd9
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-30 14:12:55 -0600

    added useEffect to check for session user id and user validations/ formatting for email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,25 @@
 class SessionsController < ApplicationController
 
+     def index
+          if session[:user_id]
+               user = User.find(session[:user_id])
+               render json: user
+          else 
+               render json: {message: "You are not logged in."}
+          end
+     end
+
      def create
           user = User.find_by_email(params[:email])
-          if user.authenticate(params[:password])
+          if user.authenticate!(params[:password])
                session[:user_id] = user.id
                render json: user
           else 
                render json: {error: "Failed to Log in"}
           end
+
+     rescue ActiveRecord::RecordInvalid => e
+          render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
      end
 
 end
--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,29 @@ import './App.css';
 import Header from './Components/Header';
 import Signup from './Components/Signup'
 import Login from './Components/Login'
+import {useState, useEffect} from 'react';
 import {
   Routes,
   Route
 } from "react-router-dom";
 
 function App() {
+  const [user,setUser] = useState(null);
+
+  useEffect(()=> {
+    fetch('/sessions').then(r => r.json()).then((data)=> {
+      if (data.id) {
+        setUser(data);
+      }
+    })
+  }, [])
+
+
+
   return (
     <div className="App">
       <Header />
+      {user ? <p>Welcome, {user.username}</p> : <p>Welcome, please log in</p>}
       <Routes>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/home" element={<Login />}/>
