f7c998f88e041db33f74528b82461706875272f2
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-30 10:36:35 -0600

    added submission and change functions for login, have no tested

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,13 +1,45 @@
 import React from 'react'
+import {useState} from "react";
 
-function Login() {
+
+function Login({user,setUser}) {
+     const formValues = {
+          email : "",
+          password : "",  
+     }
+     const [values, setValues] = useState(formValues);
+     const [errors,setErrors] = useState(null);
+
+     function handleInputChange(event) {
+          const name = event.target.name;
+          const value = event.target.value;
+
+          setValues({
+               ...values,
+               [name]: value,
+          }
+          )
+     }
+
+     function formSubmission() {
+          fetch("http://localhost:3000/sessions")
+          .then((r) => {
+               if (r.ok) {
+                    r.json().then((user)=> setUser(user));
+               } else {
+                    r.json().then((error)=> setErrors(error));
+               }
+          });
+          setValues(formValues);
+     }
+
      return (
           <div className="signup-login-form">
                <div id="email-pass-input">
                     <h1 className="signup-login-header">Log in to Easy Boards</h1>
-                    <form action="submit">
-                         <input type="text" className="form"placeholder="Enter email"/>
-                         <input type="password" className="form"placeholder="Enter password"/>
+                    <form action="submit" onSubmit={()=> formSubmission()}>
+                         <input type="text" name="email" onChange={(event)=> handleInputChange(event)}className="form"placeholder="Enter email" value={values.email}/>
+                         <input type="password" name="password" onChange={(event)=> handleInputChange(event)}className="form"placeholder="Enter password" value={values.password}/>
                          <input type="submit" className="active-login-signup-button" value="Login"/>
                     </form>
                </div>
