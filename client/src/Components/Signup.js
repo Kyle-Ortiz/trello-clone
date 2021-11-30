4dd5a90347ec26750f8a395f120d337c6537e06d
Kyle Ortiz <kyleaortiz@gmail.com>
2021-11-30 09:33:46 -0600

    added button and working on enabling the button conditionally

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {useState} from 'react'
 function Signup() {
-     const [email,setEmail] = useState("")
+     const [email,setEmail] = useState("");
+     const [validator,setValidator] = useState(false);
+
+     function formController(e,email) {
+          setEmail(e.target.value);
+          if (email.includes("@")) {
+               setValidator(()=> !validator);
+          }
+     }
 
      return (
           <div id="signup-form">
                <div id="email-input">
                     <h1 id="signup-header">Sign up for your account</h1>
                     <form action="submit">
-                         <input type="text" className="form"onChange={(e)=> setEmail(e.target.value)}placeholder="Enter email" value={email}/>
+                         <input type="text" className="form"onChange={(e)=> formController(e,email)}placeholder="Enter email" value={email}/>
+                         <input type="submit" id="continue-button" value="Continue" disabled={validator}/>
                     </form>
                </div>
           </div>
