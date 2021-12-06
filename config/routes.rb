d0fd3f6221a9ca413500945e8b924c32a013ca0a
Kyle Ortiz <kyleaortiz@gmail.com>
2021-12-06 10:42:42 -0600

    created card resource and ran migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cards
   resources :users
   resources :sessions, only: [:index,:create]
   # Routing logic: fallback requests for React Router.
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_043037) do
+ActiveRecord::Schema.define(version: 2021_12_06_164114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.bigint "list_id", null: false
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -30,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_11_23_043037) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cards", "lists"
 end
