5b19bef2a5a65d80bae0205b183313f203f10622
Kyle Ortiz <kyleaortiz@gmail.com>
2021-12-10 12:46:29 -0600

    added some code to projectBoard component, not sure best way to fetch projects lists and cards.

--- client/src/Components/ProjectBoard.js
+++ client/src/Components/ProjectBoard.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import List from './List'
 
 function ProjectBoard() {
+
+
+     function listMapper(lists) {
+          const lists = lists.map((list) => {
+               <List key={list.id} />
+          })
+     }
+
      return (
-          <div>
+          <div id="project-board">
                
           </div>
      )
